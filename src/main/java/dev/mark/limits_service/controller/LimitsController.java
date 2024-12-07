2c623b80388536326c80ffc398d9446e9c9a4653
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-12-07 17:49:26 +0100

    simple controller is created

---
+++ src/main/java/dev/mark/limits_service/controller/LimitsController.java
@@ -0,0 +1,15 @@
+package dev.mark.limits_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.mark.limits_service.limitsservice.bean.Limits;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(1,1000);
+    }
+}
---
+++ src/main/java/dev/mark/limits_service/limitsservice/bean/Limits.java
@@ -0,0 +1,32 @@
+package dev.mark.limits_service.limitsservice.bean;
+
+public class Limits {
+    private int minimum;
+    private int maximum;
+    
+    public Limits() {
+    }
+
+    public Limits(int minimum, int maximum) {
+        this.minimum = minimum;
+        this.maximum = maximum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    
+    
+}
