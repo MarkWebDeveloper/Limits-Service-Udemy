bff514a4c5d7778f59bea01e85ac59905f408289
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-12-07 18:02:12 +0100

    now using the application properties values

--- src/main/java/dev/mark/limits_service/bean/Limits.java
+++ src/main/java/dev/mark/limits_service/bean/Limits.java
@@ -1,4 +1,4 @@
-package dev.mark.limits_service.limitsservice.bean;
+package dev.mark.limits_service.bean;
 
 public class Limits {
     private int minimum;
--- src/main/java/dev/mark/limits_service/controller/LimitsController.java
+++ src/main/java/dev/mark/limits_service/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package dev.mark.limits_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import dev.mark.limits_service.limitsservice.bean.Limits;
+import dev.mark.limits_service.bean.Limits;
+import dev.mark.limits_service.configuration.Configuration;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+        // return new Limits(1,1000);
     }
 }
